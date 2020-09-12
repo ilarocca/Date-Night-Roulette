484d0d26ea1b3f7f2e08b7229a4493732ca69b8e
Ivan La Rocca <ivanlarocca12@gmail.com>
2020-09-12 12:06:45 -0400

    wireframe without google api functionality

---
+++ script.js
@@ -0,0 +1,128 @@
+//                ~~~~~~~~~GOOGLE API~~~~~~~~
+
+const googleApiKey = 'AIzaSyDM8WnT3OGrqhKlfIf9BVCadcu83FdAZgs'
+
+const googleBaseUrl = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
+
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+
+    return queryItems.join('&'); 
+}
+
+function getFoodResult(foodAddress, foodType) {
+    const params = {
+        query: foodType + foodAddress,
+        type: 'restaurant',
+        key: googleApiKey,
+        radius: 32500,
+
+    };
+
+    const queryString = formatQueryParams(params);
+    const url = googleBaseUrl + '?' + queryString; 
+
+    console.log(url)
+
+    fetch(proxyurl + url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => console.log(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+
+//              ~~~~~~~UNOGS API~~~~~~~
+
+const netflixBaseUrl = 'https://unogsng.p.rapidapi.com/search' 
+
+function getWatchResult(watchType, genreType) {
+    const params = {
+        genrelist: genreType,
+        type: watchType,
+        countrylist: "78",
+
+    };
+
+    const queryString = formatQueryParams(params);
+    const url = netflixBaseUrl + '?' + queryString; 
+
+    console.log(url)
+
+    fetch(url, {
+        "method": "GET",
+	    "headers": {
+		"x-rapidapi-host": "unogsng.p.rapidapi.com",
+		"x-rapidapi-key": "84d0b0e8damsha28a1dfe56f630ep1fbfedjsn7b96ad0f0af9"
+	    }
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => generateWatchResult(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function generateWatchResult(responseJson) {
+    console.log(responseJson)
+    $('#watch-results-list').empty();
+
+    const number = Math.floor(Math.random() * responseJson.results.length)
+    console.log(number);
+    console.log(responseJson.results[number].title);
+
+    $('#watch-results-list').append(
+        `
+        <li>
+        <h3>${responseJson.results[number].title}</h3>
+        <p>Description: ${responseJson.results[number].synopsis}</p>
+        <img id="movie-image" src="${responseJson.results[number].img}">
+        </li>
+        `
+    )
+}
+
+//              ~~~~~~~~ OMDb API ~~~~~~~~
+
+function getWatchResultScore(title) {
+    
+}
+
+
+//               ~~~~~~~ EVENT HANDLERS ~~~~~~
+
+function submitFoodForm() {
+    $('#js-food-form').submit(event => {
+        event.preventDefault(); 
+        const foodAddress = $('#js-address').val(); 
+        const foodType = $('#js-food-type').val();
+        getFoodResult(foodAddress, foodType);
+    });
+}
+
+function submitWatchForm() {
+    $('#js-watch-form').submit(event => {
+        event.preventDefault(); 
+        const watchType = $('#js-watch-type').val(); 
+        const genreType = $('#js-genre-type').val();
+        console.log(genreType);
+        getWatchResult(watchType, genreType);
+    });
+}
+
+$(submitFoodForm);
+$(submitWatchForm);
